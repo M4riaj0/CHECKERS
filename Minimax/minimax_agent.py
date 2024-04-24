5ad054c275beaeac269fe9212c25ee81df05dfb8
m4r14j0 <mariajose@esvyda.com>
2024-04-24 12:46:43 -0500

    changed the minimax

--- Minimax/minimax_agent.py
+++ Minimax/minimax_agent.py
@@ -7,22 +7,22 @@ import sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
-from evaluate_board import evaluate_board
+from evaluate_board import eval
 
 # Position is the current position that we are in, is a board object
 # depht is the number of moves that we are going to look ahead
 # max_player is a boolean that is true if we are the maximizing player and false if we are the minimizing player
 def minimax(position, game, depth, max_player, player):
+    player2 = 2 if player == 1 else 1
     if depth == 0 or position.get_winner() != None:
-        player2 = 2 if player == 1 else 1
-        return evaluate_board(position,player, player2), position
+        return eval(position.board,player, player2), position
     
     if max_player:
         maxEval = float('-inf')
         best_move = None
-        for move in position.get_possible_moves():
+        for move in position.board.get_possible_moves():
 
-            evaluation = minimax(position.move(move), depth-1, False, game)[0]
+            evaluation = minimax(position.move(move), game, depth-1, False, player2)[0]
             maxEval = max(maxEval, evaluation)
             if maxEval == evaluation:
                 best_move = move
@@ -32,7 +32,7 @@ def minimax(position, game, depth, max_player, player):
         minEval = float('inf')
         best_move = None
         for move in position.get_possible_moves():
-            evaluation = minimax(position.move(move), depth-1, True, game)[0]
+            evaluation = minimax(position.move(move), game, depth-1, True, player2)[0]
             minEval = min(minEval, evaluation)
             if minEval == evaluation:
                 best_move = move
@@ -42,16 +42,16 @@ def minimax(position, game, depth, max_player, player):
 def main():
     game = Game()
     counter = 0
-    for piece in game.board.pieces:
-        print("player", piece.player)
-        print("position piece::", piece.position)
-        counter += 1
-    print("counter", counter)
+    # for piece in game.board.pieces:
+    #     print("player", piece.player)
+    #     print("position piece::", piece.position)
+    #     counter += 1
+    # print("counter", counter)
     while not game.is_over():
         print(game.board)
         if game.whose_turn() == 1:
             print("Player 1")
-            move = minimax(game, 2, game.whose_turn, game)
+            move = minimax(game, game, 2, True, 1)
         # else:
         #     print("Player 2")
         #     move = random_player(game.board)
--- evaluate_board.py
+++ evaluate_board.py
@@ -4,6 +4,7 @@ import time
 
 # Later we can add the kings to our evaluation function
 def eval(board, player1, player2):
-    pieces_player1 = len(board.get_pieces_by_player(player1))
-    pieces_player2 = len(board.get_pieces_by_player(player2))
+    # pieces_player1 = len(board.searcher.get_pieces_by_player(player1))
+    pieces_player1 = len(board.searcher.get_pieces_by_player(player1))
+    pieces_player2 = len(board.searcher.get_pieces_by_player(player2))
     return pieces_player1 - pieces_player2
